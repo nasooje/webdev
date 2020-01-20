f8c658e2faab6f4c3151eb0e490b189b9179f227
nasooje <60055848+nasooje@users.noreply.github.com>
2020-01-21 06:32:37 +0900

    Add files via upload

---
+++ js.js
@@ -0,0 +1,45 @@
+﻿var winObj;
+
+
+
+function popup(){						// 팝업 객채 생성
+            var url = "popup.html";
+            var name = "popup test";
+            var option = "width = 500, height = 500, top = 100, left = 200, location = no, scrollbals = no, directories = no"
+            winObj = window.open(url, name, option);
+	
+
+	var chkClosePop = setInterval(function() {			// 팝업 닫힘 확인
+   		if(winObj.closed) {  
+        		clearInterval(chkClosePop);  
+        		deleteBt1();
+		addBt2();
+
+    }  
+}, 1000); 
+
+        }
+
+
+bt_print.addEventListener('click', function(){			// 버튼 액션리스너 등록
+	popup();
+    })
+
+
+
+function deleteBt1(){						// 버튼 생성
+	var bt_print = document.getElementById('bt_print');		// bt print 지정
+	var tr = bt_print.parentNode;				// bt print 부모노드 - tr 생성
+     	tr.parentNode.removeChild(tr);				// tr 부모의 자식 (=본인 노드 제거)
+
+}
+
+function addBt2(){	
+	var div = document.createElement('button');
+	div.innerHTML = '완료';
+	div.id = 'bt_commit';
+	document.getElementById('commit').appendChild(div);
+	bt_commit.addEventListener('click', function(){			// 버튼 생성 후 이벤트 리스너 등록
+		window.location.href = 'https://www.kospo.co.kr';
+	})
+}
---
+++ js_PopUp.js
@@ -0,0 +1,45 @@
+﻿
+var xhr = new XMLHttpRequest(); 				// XML 요청 객체 생성
+var url = 'https://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.xml?key=4b203564429385c2367fbd331c6daa7d&targetDt=20200101'; 
+
+//var svcKey = 'fz1ZkMwseMv%2BjR6FwlU7PRGLggkeUFsNFpkcvQc4R4UfrxGalhkIwa7yx4%2BzMgxgJcxeTsvJ%2B6nJuKqNDODH9A%3D%3D';
+
+
+
+// API open
+
+xhr.open('GET', url);
+
+
+xhr.onreadystatechange = function () {
+    if (this.readyState == 4) {
+	
+	myFunction();
+    }
+};
+
+xhr.send('');
+
+
+
+
+// 파싱된 xml 리드
+function myFunction() {
+	
+			var xmlDoc = xhr.responseXML;
+			console.log(xmlDoc);
+
+			var rank = xmlDoc.getElementsByTagName("rank"); 
+			var movieNm = xmlDoc.getElementsByTagName("movieNm"); 
+			var audiAcc = xmlDoc.getElementsByTagName("audiAcc"); 
+			var str = "박스오피스 순위" + "<br><br>";
+			
+	for(i=0; i < rank.length; i++) {
+		str += "순   위  :" + rank[i].childNodes[0].nodeValue + "===== <br>";
+		str += "영 화 명  : " + movieNm[i].childNodes[0].nodeValue + "<br>";
+		str += "누적관객수  : " + audiAcc[i].childNodes[0].nodeValue + "<br><br>";
+		
+	}
+	
+	document.getElementById("data_list").innerHTML = str;
+}
